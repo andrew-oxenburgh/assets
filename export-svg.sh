b53bcadeca664cfdb03cf179ae4a1929568e6f34
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-07-15 09:07:24 +1200

    init

---
+++ export-svg.sh
@@ -0,0 +1,7 @@
+
+echo "Run this from yarn"
+
+svgexport ./public/site-logo.svg ./public/favicon.ico png 100% 16:16
+svgexport ./public/site-logo.svg ./public/logo192 png 100% 192:192
+svgexport ./public/site-logo.svg ./public/logo256 png 100% 256:256
+svgexport ./public/site-logo.svg ./public/logo512 png 100% 512:512
